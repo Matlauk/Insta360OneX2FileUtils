4ced61f27e33d71cfa28ec193847d46134e5406c
Matlauk <mjlaukala@gmail.com>
2022-03-04 11:16:06 -0800

    Initial Commit

---
+++ Insta360OneX2Utils/Program.cs
@@ -0,0 +1,32 @@
+﻿
+var folderPath = Directory.GetCurrentDirectory();
+var cameraFolderPaths = Directory.EnumerateDirectories(folderPath);
+
+var idxFile = 0;
+
+Console.WriteLine("Working...");
+
+foreach (var cameraFolderPath in cameraFolderPaths)
+{
+    if (Path.GetDirectoryName(cameraFolderPath)?.StartsWith("Camera") ?? false)
+    {
+        continue;
+    }
+    
+    var fileGroups = Directory.EnumerateFiles(cameraFolderPath).GroupBy(Path.GetFileNameWithoutExtension).OrderBy(g => g.Key);
+    
+    foreach (var fileGroup in fileGroups)
+    {
+        foreach (var filePath in fileGroup)
+        {
+            var ext = Path.GetExtension(filePath);
+            var newFilename = $"{idxFile}{ext}";
+            var newFilePath = Path.Combine(folderPath, newFilename);
+            File.Move(filePath, newFilePath);
+        }
+
+        idxFile++;
+    }
+}
+
+Console.WriteLine("Complete!");
